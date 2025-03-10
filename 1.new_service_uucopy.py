ddaed13a41fefbe0c6019fa611ffacdf59ac050c
Robert Wang <wtaodlw@cn.ibm.com>
2025-03-10 16:12:16 +0800

    Merge remote-tracking branch 'origin/main'

--- 1.new_service_uucopy.py
+++ 1.new_service_uucopy.py
@@ -1,11 +1,13 @@
 import os
 import configparser
 import codecs
+
 conf = configparser.ConfigParser()
 conf.read_file(codecs.open('config.ini', "r", "utf-8-sig"))
 
 program = conf.get('production', 'program')
+service_name = conf.get('production', 'service_name')
 # 执行命令
-return_code = os.system(f'{program}\Instsrv.exe UUCopy {program}\Srvany.exe')
+return_code = os.system(f'{program}\Instsrv.exe {service_name} {program}\Srvany.exe')
 
 # print("返回码:", return_code)
